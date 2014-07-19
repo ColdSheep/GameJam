08b68ebaab3b51573a7904d32335bdb9fbe45384
Dominic Galasso <dominic@dominics-mbp.mobileguest.asu.edu>
2014-07-18 20:56:30 -0700

    adding the game manager script and player script

---
+++ jam/Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void BuildGrid(int width, int height)
+	{
+
+	}
+	
+	const float PERCENT_TREASURE = 0.4f;
+	const float PERCENT_TRAP = 0.2f;
+	const float PERCENT_EMPTY = 0.4f;
+	
+}
