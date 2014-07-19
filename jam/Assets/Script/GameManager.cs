550d0d7625fe4d3037359e583391b3e3711e88b5
Dominic Galasso <dominic@Dominics-MacBook-Pro.local>
2014-07-19 06:10:21 -0700

    generating tile grid

--- jam/Assets/Script/GameManager.cs
+++ jam/Assets/Script/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-	
+		BuildGrid(10, 10, 1.0f);
 	}
 
 	// Update is called once per frame
@@ -16,25 +16,35 @@ public class GameManager : MonoBehaviour {
 	
 	}
 
-	void BuildGrid(int width, int height)
+	void BuildGrid(int width, int height, float tileSize)
 	{
+		Debug.Log ("Creating Grid");
+
+		m_gameTiles = new List<GameObject>();
+
+		float x = 0.0f;
+		float y = 0.0f;
+		float z = 0.0f;
+
 		for(int i = 0; i < height; ++i)
 		{
 			for(int j = 0; j < width; ++j)
 			{
-				
+				GameObject aTile = (GameObject)Instantiate(Resources.Load("Tile"));
+
+				aTile.transform.position = new Vector3(x,y,z);
+				x += tileSize / 2.0f;
 			}
+
+			x = 0.0f;
+			y += tileSize / 2.0f;
 		}
 
 	}
 
-	void FindNeighborInfo()
-	{
-
-	}
-	
 	const float PERCENT_TREASURE = 0.4f;
 	const float PERCENT_TRAP = 0.2f;
 	const float PERCENT_EMPTY = 0.4f;
-	
+
+	public List<GameObject> m_gameTiles;
 }
---
+++ jam/Assets/Script/Tile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile {
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_AdjacentTiles = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddAdjacentTile(GameObject newTile)
+	{
+		m_AdjacentTiles.Add(newTile);
+	}
+	
+	private List <GameObject> m_AdjacentTiles;
+}
