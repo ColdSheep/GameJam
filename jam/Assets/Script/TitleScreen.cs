ae5e07c8fdd682e7886e83e45bcc73eaa746195b
Dominic Galasso <dominic@Dominics-MacBook-Pro.local>
2014-09-15 01:06:13 -0700

    Added Credits Screen

--- jam/Assets/Script/TitleScreen.cs
+++ jam/Assets/Script/TitleScreen.cs
@@ -20,21 +20,38 @@ namespace jam
 		{
 			if(showInstructions)
 			{
-				int boxWidth = 550;
-				int boxHeight = 325;
+				int boxWidth = 580;
+				int boxHeight = 340;
 
-				string t = "Instructions:\n You are a palentologist who's goal is to dig as many fossils as you can before you run out of funds. You do this by uncovering the blocks that contain fossils while avoiding the blocks that contain traps. Each block that you dig costs money. If the block uncovers a fossil (Green Block) then you gain a bonus and if the block uncovers a trap (Red Block) then you are penalized.\n\n" +
+				string t = "Instructions:\n You are an archaeologist looking for ancient dinosaur fossils- on a budget! Dig your way looking for the jurassic money makers by avoiding traps, which makes your budget smaller by medical bills to heal, and gaining the big dollars finding them bones! You do this by uncovering the blocks that contain fossils (Green Blocks) while avoiding the blocks that contain traps (Red Blocks).\n\n" +
 					       "The Blocks:\n On each dirt block there are two numbers. The top number in black is the number of fossils that are adjacent to the current block, The bottom number in white is the number of traps that are adjacent to the block.\n\n" +
 						   "End Game:\n The game is over when you either run out of money or choose to end the game by pressing the 'End Game' button in the upper left of the screen. The funal score is the number of fossils you revealed multiplied by 200 plus your ending fund balance.\n\n" +
 						   "Controls:\n" +
-						   "Left Arrow = Move/Dig Left\n" +
-						   "Right Arrow = Move/Dig Right\n" +
-						   "Down Arrow = Move/Dig Down";
+						   "'A' = Move Left\n" +
+						   "'D' = Move Right\n" +
+						   "Left Arrow = Dig Left\n" +
+						   "Right Arrow = Dig Right\n" +
+						   "Down Arrow = Dig Down";
+				GUI.TextArea(new Rect(Screen.width/2-boxWidth/2,Screen.height/12, boxWidth, boxHeight), t);
+
+				if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/12 + boxHeight + 10,100,40),"Back"))
+				{
+					showInstructions = false;
+				}
+			}
+			else if(showCredits)
+			{
+				int boxWidth = 400;
+				int boxHeight = 200;
+
+				string t = "Credits:\n\n" +
+						   "Programming: Dominic Galasso\n\n" +
+						   "Art & Design: Shawna Davis";
 				GUI.TextArea(new Rect(Screen.width/2-boxWidth/2,Screen.height/10, boxWidth, boxHeight), t);
 
 				if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/10 + boxHeight + 10,100,40),"Back"))
 				{
-					showInstructions = false;
+					showCredits = false;
 				}
 			}
 			else
@@ -53,10 +70,16 @@ namespace jam
 				{
 					showInstructions = true;
 				}
+
+				if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2+90,100,40),"Credits"))
+				{
+					showCredits = true;
+				}
 			}
 		}
 
 		bool showInstructions = false;
+		bool showCredits = false;
 	}
 
 }
